9c5cfeccedefbf938eb22c083510c6c117a59b66
Arthur Reshetnikov <artur@y-productive.com>
2019-12-01 19:20:55 +0200

    add mergeSort

--- algs.js
+++ algs.js
@@ -56,3 +56,40 @@ const gornerSchema = (x, list, i = 0) => {
     return list[i] + x * c(x, list, i + 1)
   }
 }
+
+const mergeSort = (arr, startIndex, endIndex) => {
+  if (startIndex < endIndex) {
+    const separationIndex = Math.floor((startIndex + endIndex) / 2)
+  
+    mergeSort(arr, startIndex, separationIndex)
+    mergeSort(arr, separationIndex + 1, endIndex)
+    merge(arr, startIndex, separationIndex, endIndex)
+  }
+}
+
+// used in mergeSort
+const merge = (arr, startIndex, separationIndex, endIndex) => {
+  const left = arr.slice(startIndex, separationIndex + 1)
+  const right = arr.slice(separationIndex + 1, endIndex + 1)
+  const iterations = left.length + right.length
+
+  for (let i = 0; i < iterations; i++) {
+    if (left.length === 0) {
+      arr[startIndex + i] = right.shift()
+
+      continue
+    }
+
+    if (right.length === 0) {
+      arr[startIndex + i] = left.shift()
+
+      continue
+    }
+
+    if (left[0] < right[0]) {
+      arr[startIndex + i] = left.shift()
+    } else {
+      arr[startIndex + i] = right.shift()
+    }
+  }
+}
